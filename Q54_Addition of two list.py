ce017ec240e4987d1f9d5c16f56234ac2d5424cb
gautamakky <gautamakkyy@gmail.com>
2025-06-09 15:35:30 +0530

    update

---
+++ Q54_Addition of two list.py
@@ -0,0 +1,10 @@
+a = list(map(int,input("Enter first list: ").split()))
+b = list(map(int,input("Enter second list: ").split()))
+c = []
+if len(a)==len(b):
+    for i in range(len(a)):
+        c.append(a[i]+b[i])
+    print(f'First list is: {a} \nSecond list is: {b} \nAddition of both list is {c}')
+
+else:
+    print("List size is not same.")
