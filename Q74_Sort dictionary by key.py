3a2a5b376e589479de83b1a6fd3ca8ee19fee20a
gautamakky <gautamakkyy@gmail.com>
2025-06-11 13:21:37 +0530

    update

--- Q74_Sort dictionary by key.py
+++ Q74_Sort dictionary by key.py
@@ -1,20 +1,18 @@
-my_dict = {
-    "John": 25,
-    "Alice": 30,
-    "Bob": 22,
-    "Charlie": 28
-}
+n = int(input("Enter how many keys you wants to enter in your dictionary: "))
+my_dict = {}
+for i in range(n):
+    key = input(f"Enter key {i+1}: ")
+    value = input(f'Enter value for {key}: ')
+    my_dict[key]=value
+print(f"Your given dictionary{my_dict}")
 
-# Step 1: Extract keys
 keys = list(my_dict.keys())
 
-# Step 2: Sort keys manually using Bubble Sort
 for i in range(len(keys)):
     for j in range(0, len(keys)-i-1):
         if keys[j] > keys[j+1]:
             keys[j], keys[j+1] = keys[j+1], keys[j]
 
-# Step 3: Create new dictionary in sorted key order
 sorted_dict = {}
 for key in keys:
     sorted_dict[key] = my_dict[key]
